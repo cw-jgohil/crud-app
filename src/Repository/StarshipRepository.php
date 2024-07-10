f0806bab2c04e2b3daae287b9dc5aa3a7b9fb36a
cw-jgohil <jgohil@codewinglet.com>
2024-07-10 14:48:18 +0530

    routes implemeted

---
+++ src/Repository/StarshipRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+use App\Model\StarShip;
+use Psr\Log\LoggerInterface;
+
+class StarshipRepository {
+    public function __construct(private LoggerInterface $logger) {}
+    public function findAll(): array
+    {
+        // $this->logger->info('Starship collection retrieved');
+        return [
+            new StarShip(
+                1,
+                'USS LeafyCruiser (NCC-0001)',
+                'Garden',
+                'Jean-Luc Pickles',
+                'taken over by Q'
+            ),
+            new Starship(
+                2,
+                'USS Espresso (NCC-1234-C)',
+                'Latte',
+                'James T. Quick!',
+                'repaired',
+            ),
+            new Starship(
+                3,
+                'USS Wanderlust (NCC-2024-W)',
+                'Delta Tourist',
+                'Kathryn Journeyway',
+                'under construction',
+            ),
+        ];
+    }
+    public function find(int $id): ?StarShip {
+                
+        foreach ($this->findAll() as $starship) {
+            if ($starship->getId() === $id) {
+                return $starship;
+            }
+        }
+        return null;
+
+    }
+}   
+
+?>
